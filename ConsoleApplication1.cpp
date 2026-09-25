b4a903dcac29eba7e9ba1bc4ad0575abf59d852f
yyehui66 <yeyuhui05@gmail.com>
2026-09-25 22:27:49 +0800

    Update ConsoleApplication1.cpp
    修复无功能时的不清屏
    加入版本号

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-
+//by leavesyh
 #include <iostream>
 #include <stdlib.h>
 #include <Windows.h>
@@ -8,11 +8,12 @@ using namespace std;
 
 int main()
 {
-	bool pdelse=true;
-	
+	bool pdelse = true;
 	while (true)
 	{
-		cout << "落叶手机工具箱" << endl;
+		pdelse = true;
+		cout << "落叶手机工具箱		by leavesyyh" << endl;
+		cout << "Pre-0.1.1" << endl;
 		cout << "请选择操作：" << endl;
 		cout << "0.退出此程序" << endl << "1.adb推送指定文件（无需mtp）" << endl << "2.adb拉取指定文件（无需mtp）" << endl << "3.设备重启" << endl << "4.设备重启Fastboot(Bootloader)" << endl << "5.设备重启至FastbootD(需要Android 10及以上版本)" << endl << "6.设备重启至Recovery" << endl << "7.设备重启至edl(部分旧设备可以)" << endl;
 		cout << "请输入数字选择：";
@@ -26,12 +27,14 @@ int main()
 		}
 		catch (const invalid_argument)
 		{
+			pdelse = false;
 			cout << "非正常数字字符，请重新输入";
 			Sleep(1500);
 			system("cls");
 		}
 		catch (const out_of_range)
 		{
+			pdelse = false;
 			cout << "数字超出范围，请重新输入";
 			Sleep(1500);
 			system("cls");
@@ -49,38 +52,22 @@ int main()
 			pdelse = false;
 			string adb_push_address_local, adb_push_address_device, adb_push_address_cmdline;
 			//adb推送文件变量
-			bool step_0, step_1;
-			step_0 = true;
-			step_1 = true;
 			system("cls");
-			while (step_0)
+
+
+			cout << "输入本地文件路径（可拖拽文件至此处）：";
+			cin >> adb_push_address_local;
+			cout << "输入设备目标路径（输入默认1目录）：";
+			cin >> adb_push_address_device;
+			if (adb_push_address_device == "1")
 			{
-				while (step_1)
-				{
-					cout << "输入本地文件路径（可拖拽文件至此处）：";
-					cin >> adb_push_address_local;
-					if (adb_push_address_local.empty())
-					{
-						cout << "你没有输入任何地址！";
-						system("cls");
-					}
-					else
-					{
-						step_1 = false;
-					}
-				}
-				cout << "输入设备目标路径（不输入默认/sdcard目录）：";
-				cin >> adb_push_address_device;
-				if (adb_push_address_device.empty())
-				{
-					adb_push_address_device = "/sdcard/";
-				}
-				adb_push_address_cmdline = "adb push " + adb_push_address_local + " " + adb_push_address_device;
-				WinExec(adb_push_address_cmdline.c_str(), SW_HIDE);
-				step_0 = false;
-				Sleep(1500);
-				system("cls");
+				adb_push_address_device = "/sdcard/";
 			}
+			adb_push_address_cmdline = "adb push " + adb_push_address_local + " " + adb_push_address_device;
+			WinExec(adb_push_address_cmdline.c_str(), SW_HIDE);
+			Sleep(1500);
+			system("cls");
+
 
 		}
 		if (cactnum == 2)
@@ -88,47 +75,16 @@ int main()
 			pdelse = false;
 			string adb_pull_address_local, adb_pull_address_device, adb_pull_address_cmdline;
 			//adb拉取文件变量
-			bool step_0, step_1, step_2;
-			step_0 = true;
-			step_1 = true;
-			step_2 = true;
 			system("cls");
-			while (step_0)
-			{
-				while (step_1)
-				{
-					cout << "输入设备文件路径：";
-					cin >> adb_pull_address_device;
-					if (adb_pull_address_device.empty())
-					{
-						cout << "你没有输入任何地址！";
-						system("cls");
-					}
-					else
-					{
-						step_1 = false;
-					}
-				}
-				while (step_2)
-				{
-					cout << "输入本地目标路径（不要磁盘根目录，除非使用管理员权限运行！）：";
-					cin >> adb_pull_address_local;
-					if (adb_pull_address_local.empty())
-					{
-						cout << "你没有输入任何地址！";
-						system("cls");
-					}
-					else
-					{
-						step_2 = false;
-					}
-				}
-				adb_pull_address_cmdline = "adb pull " + adb_pull_address_device + " " + adb_pull_address_local;
-				WinExec(adb_pull_address_cmdline.c_str(), SW_HIDE);
-				step_0 = false;
-				Sleep(1500);
-				system("cls");
-			}
+			cout << "输入设备文件路径：";
+			cin >> adb_pull_address_device;
+			cout << "输入本地目标路径（不要磁盘根目录，除非使用管理员权限运行！）：";
+			cin >> adb_pull_address_local;
+			adb_pull_address_cmdline = "adb pull " + adb_pull_address_device + " " + adb_pull_address_local;
+			WinExec(adb_pull_address_cmdline.c_str(), SW_HIDE);
+			Sleep(1500);
+			system("cls");
+
 		}
 		if (cactnum == 3)
 		{
@@ -162,12 +118,11 @@ int main()
 			Sleep(3000);
 			return 0;
 		}
-		if(pdelse)
+		if (pdelse)
 		{
 			cout << "不是已有的功能，请重新输入";
 			Sleep(1500);
 			system("cls");
 		}
-		
 	}
 }
